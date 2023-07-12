3489a98362f11dbbb59782f83918350132637144
jumz3 <kaptonov@gmail.com>
2023-07-13 02:02:07 +0300

    .

--- whale/main.py
+++ whale/main.py
@@ -6,42 +6,54 @@ from telebot.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from whale.speech import SpeechRecognizer, UnableToRecognizeError
 from whale.utils import ogg_to_wav
 from whale.settings import (
-    ERROR_TEXT,
-    BOT_STICKER,
-    BOT_TEXT_MARKUP,
-    AUTHOR_GITHUB_TEXT,
-    AUTHOR_GITHUB_URL,
-    SPEECH_RECOGNIZER_LANGUAGE
+    VOICE_MESSAGE_LANGUAGE,
+    PARSE_MODE,
+    DISABLE_NOTIFICATION
 )
+from whale.resources import Bot, Author, ErrorText
+
 
 bot = AsyncTeleBot(
     token=os.environ.get("TOKEN"),
-    parse_mode="html",
-    disable_notification=True
+    parse_mode=PARSE_MODE,
+    disable_notification=DISABLE_NOTIFICATION
 )
-recognizer = SpeechRecognizer()
+speech_recognizer = SpeechRecognizer()
 
 
 @bot.message_handler(commands="start", chat_types="private")
-async def on_help_command_received(message: Message):
-    markup = InlineKeyboardMarkup()
-    markup.add(InlineKeyboardButton(AUTHOR_GITHUB_TEXT, AUTHOR_GITHUB_URL))
+async def on_start_command_received(message: Message):
+    """
+    Triggers when /start message received in private chat.
+    Bot sends sticker and description of itself
+    :param message: telebot.types.Message
+    """
 
-    await bot.send_sticker(message.chat.id, BOT_STICKER)
-    await bot.send_message(message.chat.id, BOT_TEXT_MARKUP, reply_markup=markup)
+    markup = InlineKeyboardMarkup()
+    markup.add(InlineKeyboardButton(Author.GITHUB_TEXT, Author.GITHUB_URL))
+
+    await bot.send_sticker(message.chat.id, Bot.STICKER_ID)
+    await bot.send_message(message.chat.id, Bot.DESCRIPTION, reply_markup=markup)
 
 
 @bot.message_handler(content_types="voice", chat_types="private")
 async def on_voice_message_received(message: Message):
+    """
+    Triggers when voice message received in private chat.
+    Bot replies with recognized text in this message,
+    if it cannot recognize, replies with error text.
+    :param message: telebot.types.Message
+    """
+
     try:
         voice_file = await bot.get_file(message.voice.file_id)
         voice_ogg_bytes = await bot.download_file(voice_file.file_path)
-        text = recognizer.recognize_from_wav(
+        text = speech_recognizer.recognize_from_wav(
             ogg_to_wav(BytesIO(voice_ogg_bytes), BytesIO()),
-            SPEECH_RECOGNIZER_LANGUAGE
+            VOICE_MESSAGE_LANGUAGE
         )
     except UnableToRecognizeError:
-        text = ERROR_TEXT
+        text = ErrorText.UNABLE_TO_RECOGNIZE
 
     await bot.reply_to(message, text)
 
--- whale/settings.py
+++ whale/settings.py
@@ -1,13 +1,5 @@
-"""Settings module to configure bot"""
+"""Module to configure bot"""
 
-SPEECH_RECOGNIZER_LANGUAGE = "ru-RU"
-
-AUTHOR_GITHUB_TEXT = "Посетить Github разработчика"
-AUTHOR_GITHUB_URL = "https://github.com/jumz33"
-
-BOT_NAME = "Say@Whale"
-BOT_DESCRIPTION = "Привет! Просто отправь мне голосовое сообщение и я переведу его в текст"
-BOT_TEXT_MARKUP = f"<b>{BOT_NAME}</b>\n{BOT_DESCRIPTION}"
-BOT_STICKER = "CAACAgEAAxkBAAEJfP5kmYVTv38j9YlIAwI2PT-gLNGtLAACGwMAArAHGESRLvZwzZJ9si8E"
-
-ERROR_TEXT = "Не могу распознать слова в сообщении."
+VOICE_MESSAGE_LANGUAGE = "ru-RU"
+PARSE_MODE = "html"
+DISABLE_NOTIFICATION = True
